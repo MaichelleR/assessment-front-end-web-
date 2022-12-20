b213b96983039032e0ffa46840d7773cf9209595
unknown <maichellereynolds2@gmail.com>
2022-12-19 18:58:51 -0800

    final commit

---
+++ profile.js
@@ -0,0 +1,27 @@
+const colorButton = document.getElementById('color');
+
+colorButton.addEventListener('click', () => {
+    alert('fav color is black!');
+
+});
+console.log('fav color is black!')
+
+
+
+const favPlaceButton = document.getElementById('place');
+
+favPlaceButton.addEventListener('click', () => {
+    alert('home');
+
+});
+console.log('home')
+
+const favoriteRitual = document.getElementById('ritual');
+
+favoriteRitual.addEventListener('click', () => {
+    alert('cooking');
+
+});
+console.log('cooking')
+
+
